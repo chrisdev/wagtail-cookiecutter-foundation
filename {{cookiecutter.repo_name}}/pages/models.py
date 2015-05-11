8c8edadf38a2622f24b3390349f1ab28ccdc1554
Christopher Clarke <cclarke@chrisdev.com>
2015-05-11 08:59:39 -0400

    Move the navigation tags to utils

--- {{cookiecutter.repo_name}}/pages/models.py
+++ {{cookiecutter.repo_name}}/pages/models.py
@@ -12,103 +12,104 @@ from wagtail.wagtailadmin.edit_handlers import (
 from wagtail.wagtailsearch import index
 from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
 
+from utils import LinkFields, ContactFields, RelatedLink, CarouselItem
 
-class LinkFields(models.Model):
-    link_external = models.URLField("External link", blank=True)
-    link_page = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        related_name='+'
-    )
-    link_document = models.ForeignKey(
-        'wagtaildocs.Document',
-        null=True,
-        blank=True,
-        related_name='+'
-    )
+#class LinkFields(models.Model):
+    #link_external = models.URLField("External link", blank=True)
+    #link_page = models.ForeignKey(
+        #'wagtailcore.Page',
+        #null=True,
+        #blank=True,
+        #related_name='+'
+    #)
+    #link_document = models.ForeignKey(
+        #'wagtaildocs.Document',
+        #null=True,
+        #blank=True,
+        #related_name='+'
+    #)
 
-    @property
-    def link(self):
-        if self.link_page:
-            return self.link_page.url
-        elif self.link_document:
-            return self.link_document.url
-        else:
-            return self.link_external
+    #@property
+    #def link(self):
+        #if self.link_page:
+            #return self.link_page.url
+        #elif self.link_document:
+            #return self.link_document.url
+        #else:
+            #return self.link_external
 
-    panels = [
-        FieldPanel('link_external'),
-        PageChooserPanel('link_page'),
-        DocumentChooserPanel('link_document'),
-    ]
+    #panels = [
+        #FieldPanel('link_external'),
+        #PageChooserPanel('link_page'),
+        #DocumentChooserPanel('link_document'),
+    #]
 
-    class Meta:
-        abstract = True
-
-
-class ContactFields(models.Model):
-    telephone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(blank=True)
-    address_1 = models.CharField(max_length=255, blank=True)
-    address_2 = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=255, blank=True)
-    country = models.CharField(max_length=255, blank=True)
-    post_code = models.CharField(max_length=10, blank=True)
-
-    panels = [
-        FieldPanel('telephone'),
-        FieldPanel('email'),
-        FieldPanel('address_1'),
-        FieldPanel('address_2'),
-        FieldPanel('city'),
-        FieldPanel('country'),
-        FieldPanel('post_code'),
-    ]
-
-    class Meta:
-        abstract = True
+    #class Meta:
+        #abstract = True
 
 
-# Carousel items
+#class ContactFields(models.Model):
+    #telephone = models.CharField(max_length=20, blank=True)
+    #email = models.EmailField(blank=True)
+    #address_1 = models.CharField(max_length=255, blank=True)
+    #address_2 = models.CharField(max_length=255, blank=True)
+    #city = models.CharField(max_length=255, blank=True)
+    #country = models.CharField(max_length=255, blank=True)
+    #post_code = models.CharField(max_length=10, blank=True)
 
-class CarouselItem(LinkFields):
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    embed_url = models.URLField("Embed URL", blank=True)
-    caption = RichTextField(blank=True)
+    #panels = [
+        #FieldPanel('telephone'),
+        #FieldPanel('email'),
+        #FieldPanel('address_1'),
+        #FieldPanel('address_2'),
+        #FieldPanel('city'),
+        #FieldPanel('country'),
+        #FieldPanel('post_code'),
+    #]
 
-    panels = [
-        ImageChooserPanel('image'),
-        FieldPanel('embed_url'),
-        FieldPanel('caption'),
-        MultiFieldPanel(LinkFields.panels, "Link"),
-    ]
-
-    class Meta:
-        abstract = True
-
-
-# Related links
-
-class RelatedLink(LinkFields):
-    title = models.CharField(max_length=255, help_text="Link title")
-
-    panels = [
-        FieldPanel('title'),
-        MultiFieldPanel(LinkFields.panels, "Link"),
-    ]
-
-    class Meta:
-        abstract = True
+    #class Meta:
+        #abstract = True
 
 
-# Concrete Pages
+## Carousel items
+
+#class CarouselItem(LinkFields):
+    #image = models.ForeignKey(
+        #'wagtailimages.Image',
+        #null=True,
+        #blank=True,
+        #on_delete=models.SET_NULL,
+        #related_name='+'
+    #)
+    #embed_url = models.URLField("Embed URL", blank=True)
+    #caption = RichTextField(blank=True)
+
+    #panels = [
+        #ImageChooserPanel('image'),
+        #FieldPanel('embed_url'),
+        #FieldPanel('caption'),
+        #MultiFieldPanel(LinkFields.panels, "Link"),
+    #]
+
+    #class Meta:
+        #abstract = True
+
+
+## Related links
+
+#class RelatedLink(LinkFields):
+    #title = models.CharField(max_length=255, help_text="Link title")
+
+    #panels = [
+        #FieldPanel('title'),
+        #MultiFieldPanel(LinkFields.panels, "Link"),
+    #]
+
+    #class Meta:
+        #abstract = True
+
+
+## Concrete Pages
 
 class HomePageContentItem(Orderable, LinkFields):
     page = ParentalKey('pages.HomePage', related_name='content_items')
