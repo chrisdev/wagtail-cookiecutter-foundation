7500dcb6bafdc3e52aea0776145bade41e9db27c
Parbhat Puri <parbhatpuri17@gmail.com>
2015-05-28 22:02:11 +0530

    custom fixture script modified

--- {{cookiecutter.repo_name}}/pages/management/commands/load_initial_data.py
+++ {{cookiecutter.repo_name}}/pages/management/commands/load_initial_data.py
@@ -10,7 +10,9 @@ class Command(NoArgsCommand):
         fixtures_dir = os.path.join(settings.PROJECT_ROOT, 'pages', 'fixtures')
         fixture_file = os.path.join(fixtures_dir, 'initial_data.json')
         image_src_dir = os.path.join(fixtures_dir, 'images')
-        image_dest_dir = os.path.join(settings.MEDIA_ROOT, 'original_images')
+        image_src_dir_or = os.path.join(fixtures_dir, 'original_images')
+        image_dest_dir_or = os.path.join(settings.MEDIA_ROOT, 'original_images')
+        image_dest_dir = os.path.join(settings.MEDIA_ROOT, 'images')
 
         call_command('loaddata', fixture_file, verbosity=0)
 
@@ -19,3 +21,9 @@ class Command(NoArgsCommand):
 
         for filename in os.listdir(image_src_dir):
             shutil.copy(os.path.join(image_src_dir, filename), image_dest_dir)
+
+        if not os.path.isdir(image_dest_dir_or):
+            os.makedirs(image_dest_dir_or)
+
+        for filename in os.listdir(image_src_dir_or):
+            shutil.copy(os.path.join(image_src_dir_or, filename), image_dest_dir_or)
