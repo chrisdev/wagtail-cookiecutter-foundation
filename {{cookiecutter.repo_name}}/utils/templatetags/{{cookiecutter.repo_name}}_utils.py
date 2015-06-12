48a62a8f1c7e6258a7177e2dc71eee223f7d59b0
Lendl Smith <lendl.smith@gmail.com>
2015-06-12 12:36:31 -0400

    news feed added

--- {{cookiecutter.repo_name}}/utils/templatetags/{{cookiecutter.repo_name}}_utils.py
+++ {{cookiecutter.repo_name}}/utils/templatetags/{{cookiecutter.repo_name}}_utils.py
@@ -1,5 +1,8 @@
 from django import template
+from django.conf import settings
+from django.template.loader import get_template
 from contact.models import ContactPage
+from blog.models import BlogPage
 
 register = template.Library()
 
@@ -15,3 +18,12 @@ def get_contact_fields(context):
 
     except IndexError:
         return {}
+        
+@register.inclusion_tag('blog/includes/blog_post_listing.html',takes_context=True)
+def news_feed(context, count=2):
+    blogs = BlogPage.objects.filter(live=True).order_by('-date')
+    return {
+        'blogs': blogs[:count],
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
