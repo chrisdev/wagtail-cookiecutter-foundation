626e9726ba393136599488b41b7ae0b31a0ad5ff
parbhat <parbhatpuri17@gmail.com>
2015-08-05 00:21:27 +0530

    Nodejs support on vagrant box - Issue #103

--- {{cookiecutter.repo_name}}/vagrant/provision.sh
+++ {{cookiecutter.repo_name}}/vagrant/provision.sh
@@ -22,7 +22,7 @@ su - vagrant -c "echo $PROJECT_DIR > $VIRTUALENV_DIR/.project"
 
 
 # Install PIP requirements
-su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements.txt"
+su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements/dev.txt"
 
 
 # Set execute permissions on manage.py as they get lost if we build from a zip file
@@ -33,6 +33,15 @@ chmod a+x $PROJECT_DIR/manage.py
 su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py migrate --noinput && \
                  $PYTHON $PROJECT_DIR/manage.py update_index"
 
+# Load Fixtures
+su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py load_initial_data"
+
+# Install NodeJS and Bower
+apt-get install python-software-properties
+add-apt-repository ppa:chris-lea/node.js
+apt-get -y update
+apt-get install -y nodejs
+npm install -g bower
 
 # Add a couple of aliases to manage.py into .bashrc
 cat << EOF >> /home/vagrant/.bashrc
