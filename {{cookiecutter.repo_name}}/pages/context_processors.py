f4dc9b728d0f343700855d1db63cce9f823658af
Lendl Smith <lendl.smith@gmail.com>
2015-09-18 14:59:08 -0400

    no message

--- {{cookiecutter.repo_name}}/pages/context_processors.py
+++ {{cookiecutter.repo_name}}/pages/context_processors.py
@@ -1,7 +1,11 @@
 from django.contrib.sites.models import get_current_site
-
+from urlparse import urljoin
 def site_url(request):
     scheme = 'https' if request.is_secure() else 'http'
+    site = get_current_site(request)
+    #domain = "{}://{}".format(scheme, site.domain)
 
-    domain = ''.join([scheme, '://', get_current_site(request).domain])
-    return { 'site_url': domain}
+    return {
+        'site_url':  "{}://{}".format(scheme, site.domain),
+        'site_name': site.name
+    }
