8680eac07c50546968c1525642cab41c1c99a6b3
Parbhat Puri <parbhatpuri17@gmail.com>
2015-10-18 16:39:11 +0530

    Remove urlparse import as not used and also renamed in Python 3 to urllib.parse

--- {{cookiecutter.repo_name}}/pages/context_processors.py
+++ {{cookiecutter.repo_name}}/pages/context_processors.py
@@ -1,5 +1,5 @@
 from django.contrib.sites.shortcuts import get_current_site
-from urlparse import urljoin
+
 def site_url(request):
     scheme = 'https' if request.is_secure() else 'http'
     site = get_current_site(request)
