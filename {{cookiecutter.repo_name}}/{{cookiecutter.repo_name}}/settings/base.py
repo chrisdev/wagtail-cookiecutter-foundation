2213813dc4ff248cd6e20848f2a38932d027c552
Parbhat Puri <parbhatpuri17@gmail.com>
2016-07-08 12:35:52 +0530

    Address Issue #277

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -77,6 +77,10 @@ INSTALLED_APPS = (
     'wagalytics',
     'wagtailfontawesome',
 
+    {% if cookiecutter.use_django_cachalot == "y" %}
+    'cachalot',
+    {% endif %}
+
     'utils',
     'pages',
     'blog',
@@ -209,17 +213,19 @@ TWITTER_DESCRIPTION = ''
 
 # Use Redis as the cache backend for extra performance
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'redis_cache.cache.RedisCache',
-#         'LOCATION': '127.0.0.1:6379',
-#         'KEY_PREFIX': '{{ cookiecutter.repo_name }}',
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-#         }
-#     }
-# }
+{% if cookiecutter.use_django_cachalot == "y" %}
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+{% endif %}
 
 # Wagtail settings
 
