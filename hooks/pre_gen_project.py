b07b08292673784b9ee2159ebe51bb617e7a85cf
Christopher Clarke <cclarke@chrisdev.com>
2016-08-12 14:35:56 -0400

    Fix handler permissioning

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,5 +1,4 @@
 project_slug = '{{ cookiecutter.project_slug }}'
-
+msg = 'Project Slug should be valid Python identifier!'
 if hasattr(project_slug, 'isidentifier'):
-    assert project_slug.isidentifier(), 'Repo should be valid Python identifier!'
-
+    assert project_slug.isidentifier(), msg
