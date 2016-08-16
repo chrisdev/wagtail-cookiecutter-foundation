d4df8d8c3013d715ddcdb1578ca35c3dbf86ab8e
Parbhat Puri <parbhatpuri17@gmail.com>
2016-08-16 12:40:46 +0530

    fix: django.core.management.NoArgsCommand is removed in django 1.10 - Issue #364

--- {{cookiecutter.project_slug}}/pages/management/commands/drop_initial_user.py
+++ {{cookiecutter.project_slug}}/pages/management/commands/drop_initial_user.py
@@ -1,11 +1,11 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(BaseCommand):
+    def handle(self, **options):
         try:
             User.objects.get(username='admin').delete()
         except ObjectDoesNotExist:
-            print "Already Removed"
+            print("Already Removed")
