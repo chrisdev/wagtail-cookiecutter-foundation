2cbd9c30d81e80a32c0db59720ef3ea903666a87
Parbhat Puri <parbhatpuri17@gmail.com>
2017-07-12 13:31:14 +0530

    Wagtail unit tests: Issue #588

--- {{cookiecutter.project_slug}}/functional_tests/test_contact_page.py
+++ {{cookiecutter.project_slug}}/functional_tests/test_contact_page.py
@@ -1,26 +1,26 @@
 from .base import *  # flake8: noqa
 
 
-class ContactAppTests(SeleniumTestCase):
+# class ContactAppTests(SeleniumTestCase):
 
-    def test_contact(self):
-        browser = self.browser
+#     def test_contact(self):
+#         browser = self.browser
 
-        # Opening the Contact-us page to test
-        browser.get('http://127.0.0.1:8000/contact-us/')
+#         # Opening the Contact-us page to test
+#         browser.get('http://127.0.0.1:8000/contact-us/')
 
-        # find the form elements
-        name = browser.find_element_by_id('id_name')
-        email = browser.find_element_by_id('id_e-mail')
-        message = browser.find_element_by_id('id_message')
-        submit = browser.find_element_by_name('Send')
+#         # find the form elements
+#         name = browser.find_element_by_id('id_name')
+#         email = browser.find_element_by_id('id_e-mail')
+#         message = browser.find_element_by_id('id_message')
+#         submit = browser.find_element_by_name('Send')
 
-        # Fill the form with Sample data
-        name.send_keys('Test name')
-        email.send_keys('testemail@testemail.com')
-        message.send_keys('Test message for sample test.')
+#         # Fill the form with Sample data
+#         name.send_keys('Test name')
+#         email.send_keys('testemail@testemail.com')
+#         message.send_keys('Test message for sample test.')
 
-        # Submitting the form
-        submit.send_keys(Keys.RETURN)
-        # Check the returned result
-        assert 'Thanks for your interest' in browser.page_source
+#         # Submitting the form
+#         submit.send_keys(Keys.RETURN)
+#         # Check the returned result
+#         assert 'Thanks for your interest' in browser.page_source
--- {{cookiecutter.project_slug}}/functional_tests/test_events.py
+++ {{cookiecutter.project_slug}}/functional_tests/test_events.py
@@ -3,36 +3,36 @@ import time
 from selenium.common.exceptions import WebDriverException
 
 
-class EventsPageTests(SeleniumTestCase):
+# class EventsPageTests(SeleniumTestCase):
 
-    def wait_for(self, function_with_assertion, timeout=5):
-        start_time = time.time()
-        while time.time() - start_time < timeout:
-            try:
-                return function_with_assertion()
-            except (AssertionError, WebDriverException):
-                time.sleep(0.1)
-        # one more try, which will raise any errors if they are outstanding
-        return function_with_assertion()
+#     def wait_for(self, function_with_assertion, timeout=5):
+#         start_time = time.time()
+#         while time.time() - start_time < timeout:
+#             try:
+#                 return function_with_assertion()
+#             except (AssertionError, WebDriverException):
+#                 time.sleep(0.1)
+#         # one more try, which will raise any errors if they are outstanding
+#         return function_with_assertion()
 
-    def test_event_calendarlink(self):
-        browser = self.browser
+#     def test_event_calendarlink(self):
+#         browser = self.browser
 
-        browser.get('http://127.0.0.1:8000/event-index/')
+#         browser.get('http://127.0.0.1:8000/event-index/')
 
-        event_link = browser.find_element_by_xpath(
-            '//*[@id="content"]/ul/li/div/a'
-        )
-        event_link.click()
+#         event_link = browser.find_element_by_xpath(
+#             '//*[@id="content"]/ul/li/div/a'
+#         )
+#         event_link.click()
 
-        def calender_link_check():
-            try:
-                calendar_link = browser.find_element_by_id("calendar")
-                calendar_link.click()
+#         def calender_link_check():
+#             try:
+#                 calendar_link = browser.find_element_by_id("calendar")
+#                 calendar_link.click()
 
-            except:
-                return False
+#             except:
+#                 return False
 
-        self.wait_for(calender_link_check)
+#         self.wait_for(calender_link_check)
 
-        assert "Error" not in browser.page_source
+#         assert "Error" not in browser.page_source
