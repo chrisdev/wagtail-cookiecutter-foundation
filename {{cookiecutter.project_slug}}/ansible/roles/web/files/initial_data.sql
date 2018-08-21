a5a0b8f7d74053241ca6a31c9972033ba83f82c5
Lendl Smith <lendl.smith@gmail.com>
2018-08-21 14:18:54 -0400

    wagtail 2.1.2

--- {{cookiecutter.project_slug}}/ansible/roles/web/files/initial_data.sql
+++ {{cookiecutter.project_slug}}/ansible/roles/web/files/initial_data.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.9
--- Dumped by pg_dump version 10.4
+-- Dumped from database version 9.6.10
+-- Dumped by pg_dump version 10.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
