5f56a04217232cae80cf3bf439c667d897beae9d
Lendl Smith <lendl.smith@gmail.com>
2019-06-15 16:52:31 -0400

    login redirect fix

--- {{cookiecutter.project_slug}}/users/adapters.py
+++ {{cookiecutter.project_slug}}/users/adapters.py
@@ -8,6 +8,9 @@ class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
+    def get_login_redirect_url(self, request):
+        path = "/"
+        return path.format(username=request.user.username)
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request, sociallogin):
