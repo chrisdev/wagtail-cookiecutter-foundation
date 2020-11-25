81b8136171339956590b01b9afb503b49ecc71ff
dennereed <denne.reed@gmail.com>
2020-11-25 08:25:01 -0800

    update to work with custom User

--- {{cookiecutter.project_slug}}/pages/management/commands/drop_initial_user.py
+++ {{cookiecutter.project_slug}}/pages/management/commands/drop_initial_user.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as AuthUser
 from django.core.exceptions import ObjectDoesNotExist
 
 
