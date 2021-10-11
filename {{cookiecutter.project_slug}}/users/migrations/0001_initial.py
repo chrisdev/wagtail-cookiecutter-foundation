02df5b08951848cc0c0e6b732fa7ee23b3358536
Lendl Smith <lendl.smith@gmail.com>
2021-10-11 09:57:27 -0400

    no message

--- {{cookiecutter.project_slug}}/users/migrations/0001_initial.py
+++ {{cookiecutter.project_slug}}/users/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('name', models.CharField(default='', max_length=255, verbose_name='Full name')),
+                ('name', models.CharField(default='', max_length=255, verbose_name='Full Name')),
                 ('address', models.TextField(default='', verbose_name='Address')),
                 ('city', models.CharField(default='', max_length=255, verbose_name='City or Town')),
                 ('state', models.CharField(blank=True, default='', max_length=255, verbose_name='State/Province')),
